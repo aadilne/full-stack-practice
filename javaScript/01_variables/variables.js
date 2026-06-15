0146ec67c7c10f25fadffa9ed26803d9b94b8e4c
Aadil Nezam <aadilnezam7782@gmail.com>
2026-06-15 09:21:22 -0700

    javaScript variables practice

---
+++ javaScript/01_variables/variables.js
@@ -0,0 +1,77 @@
+
+let name = "Aadil";
+console.log(name);
+
+var city = "Ara";
+console.log(city);
+
+let age = 23;
+console.log(age);
+
+const country = "India";
+console.log(country);
+
+
+let age1 = 20;
+age1 = 25;               // we can update let variables type
+console.log(age1);
+
+
+const DOB = 25012005;
+// DOB = 26012005          // we can't update const variables type
+console.log(typeof(DOB));
+console.log(DOB);
+
+
+// Variables Naming Rules
+// let name = "Aadil";
+// let age1 = 20;
+// let firstName = "Aadil";
+// let _salary = 10000;
+// let $price = 500;
+
+
+/*
+let 1age = 20;
+let first name = "Aadil";   //wrong variables Nameing ❌
+let let = 10;
+*/
+
+
+let name_A = "Aadil Nezam",
+    age_A = 24,
+    city_A = "Patna";
+
+    console.log(name_A);
+    console.log(age_A);
+    console.log(city_A);
+    console.log(name_A , age_A , city_A);
+
+
+
+    // variables update
+let score = 50;
+console.log(score);
+score = 100;
+console.log(score);
+
+
+  // variables update
+let a = 7782;
+let b = a;
+console.log(a);
+console.log(b);
+
+
+let nameC = "Manoj Kumar";
+console.log("Your Name is " + nameC);
+
+
+let numA = 10;
+let numB = 20;
+console.log(numA + numB);
+
+let name_C = "Aman",
+    age_C = 23;
+
+console.log(`My name is ${name_C} and my age is ${age_C} done`);
