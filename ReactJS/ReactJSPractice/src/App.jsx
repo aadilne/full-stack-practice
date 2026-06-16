b869cd78ff5ac2d119fea0720837858cfd6e0f8f
Aadil Nezam <aadilnezam7782@gmail.com>
2026-06-16 05:50:40 -0700

    practice ReactJS day1

---
+++ ReactJS/ReactJSPractice/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Demo from './component/Demo'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>react</h1>
+      <Demo />
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ ReactJS/ReactJSPractice/src/component/Demo.jsx
@@ -0,0 +1,11 @@
+
+function    Demo(){
+
+    return(
+        <>
+        <h2>My first ReactJS learning journey</h2>
+        </>
+    )
+}
+
+export default Demo
