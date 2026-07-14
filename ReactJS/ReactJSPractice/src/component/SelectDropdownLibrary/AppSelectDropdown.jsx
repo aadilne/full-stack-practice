16ebab9407bd8080532649cf06d801384a7c0e06
Aadil Nezam <aadilnezam7782@gmail.com>
2026-07-14 22:05:29 +0530

    Practice Select library code in ReactJS

--- ReactJS/ReactJSPractice/src/component/SelectDropdownLibrary/AppSelectDropdown.jsx
+++ ReactJS/ReactJSPractice/src/component/SelectDropdownLibrary/AppSelectDropdown.jsx
@@ -1,8 +1,6 @@
 
 import SelectDropdown from "./SelectDropdown"
 
-import Select from "react-select"
-
 function   AppSelectDropdown(){
 
 
--- ReactJS/ReactJSPractice/src/component/SelectDropdownLibrary/SelectDropdown.jsx
+++ ReactJS/ReactJSPractice/src/component/SelectDropdownLibrary/SelectDropdown.jsx
@@ -1,11 +1,51 @@
+import Select from 'react-select'
+import { useState } from 'react'
 
 function    SelectDropdown(){
+    const [fruit , setFruit] = useState(null)
+
+    const options = [
+        {value : "chocolate" , label : "chocolate"},
+        {value : "straberry" , label : "straberry"},
+        {value : "vanila" , label : "vanila"},
+    ]
+
+    const customStyles ={
+        control : (base) => ({
+            ...base ,
+            borderColor : "red",
+            borderWidth : "4px",
+            padding : "5px",
+        }),
+
+        option : (base) => ({
+            ...base,
+            color : "blue",
+            backgoundColor : "white",
+            fontWeight : "bold"
+        })
+    };
 
 
     return(
-        <div>
+        <div style={{width : "300px"}}>
             
             <h1>Select Dropdown</h1>
+
+            < Select options = {options} value={fruit} 
+
+            onChange ={(selected) =>  { setFruit(selected)
+                console.log(selected)
+            }} 
+            //isMulti
+            placeholder="Select Your Fruit"
+            isClearable
+            isDisabled={false}
+            defaultValue={options[1]}
+            isSearchable={true}
+            styles={customStyles}/>
+
+            <p>{fruit?.label}</p>
 
         </div>
     )
