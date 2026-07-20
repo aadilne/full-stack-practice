e5731069388c0ebc1d32cc73d0011191c9f15850
Aadil Nezam <aadilnezam7782@gmail.com>
2026-07-20 21:52:54 +0530

    import reactjs library

--- ReactJS/ReactJSPractice/src/component/CURD/Pages/AddEditUser.jsx
+++ ReactJS/ReactJSPractice/src/component/CURD/Pages/AddEditUser.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useNavigate , useParams } from "react-router-dom";
+import Api from "../Api/Axios";
+
+
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import {userSchema} from "../Validations/UsersShema";
 
 function AddEditUser(){
 
 
     return (
         <div>
-<h1>edit user </h1>
+        <h1>edit user </h1>
         </div>
     )
 }
