970d77907805dc4ba1c976553181c12fa3b8004b
Aadil Nezam <aadilnezam7782@gmail.com>
2026-07-25 19:24:59 +0530

    practice loops in JS

---
+++ javaScript/04_Loops/loops.js
@@ -0,0 +1,49 @@
+
+
+console.log("loop practice");
+
+// for loop
+
+for(let i = 0 ; i<=5 ; i++){
+    console.log("i will not talk in class");
+}
+
+
+{
+    for(let i = 5; i <= 8; i++){
+    console.log(i);
+    }
+
+}
+
+//10 to 1 resverse print
+{
+    for(let i=10 ; i>=1; i--){
+        console.log(i);
+    }
+
+}
+
+// 1 to 20 even number
+
+{
+    for(let i = 0 ; i<=20; i++){
+        if (i %2===0){
+            console.log(i);
+        }
+    }
+
+}
+
+// 1 to 20 odd number
+{
+for (let i = 0; i<=20; i++){
+    if (i%2 !== 0){
+        console.log(i);
+    }
+}
+
+}
+
+
+
