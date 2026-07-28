b6612866284d081c3e4f038028916adb4a8b9256
Aadil Nezam <aadilnezam7782@gmail.com>
2026-07-28 14:03:56 +0530

    creat calculator

--- NodeJS/03_Parsing_Request/Calculator/handler.js
+++ NodeJS/03_Parsing_Request/Calculator/handler.js
@@ -1,4 +1,5 @@
 
+const {sumRequestHandler} = require('./sum');
 
 const requestHandler = ( req ,res ) =>{
 
@@ -18,6 +19,28 @@ const requestHandler = ( req ,res ) =>{
             
         `);
         return res.end();
+    } else if (req.url.toLowerCase() === "/calculator"){
+        res.setHeader('Content-Type' , 'text/html');
+        res.write(`
+        <html>
+        <head> <title> Practice set  </title>  </head>
+        <body>
+         <h1> Calculator  </h1> 
+         <form  action ="/calculator-result" method ="POST"> 
+         <input type="text"  placeholder ="first num" name="first" />
+         <input type="text"  placeholder ="second num" name="sec num" />
+         <input type="submit" value ="sum">
+
+         </form>
+         
+        </body>
+        </html>  
+        
+        `)
+        return res.end();
+
+    } else if (req.url.toLowerCase() ==="/calculate-result"  && req.method === 'POST') {
+       return sumRequestHandler(req ,res);
     }
 
     res.setHeader('Content-Type' , 'text/html');
