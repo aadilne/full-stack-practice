e79af45647fcb938b83a06f344466e26380b1888
Aadil Nezam <aadilnezam7782@gmail.com>
2026-08-06 19:55:05 +0530

    String practice

---
+++ javaScript/06_String/string.js
@@ -0,0 +1,16 @@
+{
+let a = "22";
+let b = 22;
+let c = "Aadil22";
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+}
+
+{
+    let name = "Aadil";
+let age = 22;
+
+console.log(`My name is ${name} and age is ${age}`);
+}
