a66090387c62fc031385e23e05d0764986e5227a
Aadil Nezam <aadilnezam7782@gmail.com>
2026-08-29 23:56:15 +0530

    change code

--- javaScript/10_Mini_Project_DOM/02_List_And_Search/List-And-Search.js
+++ javaScript/10_Mini_Project_DOM/02_List_And_Search/List-And-Search.js
@@ -11,30 +11,32 @@ const arr =[
     },
 
      {
-        profileUrl : "https://images.news18.com/ibnlive/uploads/2017/12/Salman-Khan6.jpg",
+        profileUrl : "https://upload.wikimedia.org/wikipedia/commons/6/6e/Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg?utm_source=en.wikipedia.org&utm_campaign=index&utm_content=original",
         name : "Sarukh Khan",
-        email : "salmankahn@gmail.com"
+        email : "Sarukhkahn@gmail.com"
     },
 
      {
-        profileUrl : "https://images.news18.com/ibnlive/uploads/2017/12/Salman-Khan6.jpg",
+        profileUrl : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgSZ8TS1uLxLCNkHxpea7e1IasdfJF51KenXsOdtKf7Q&s=10",
         name : "Amir Khan",
-        email : "salmankahn@gmail.com"
+        email : "Amirkahn@gmail.com"
     }
 ]
 
 arr.map((item) =>{
+
+    let {profileUrl , name , email} = item;
     
     let divElement = document.createElement("div");
     divElement.className = "userItem";
     divElement.innerHTML = `<div class="image">
-                <img src="https://images.news18.com/ibnlive/uploads/2017/12/Salman-Khan6.jpg" alt="error loding image">
+                <img src="${profileUrl}" alt="error loding image">
                 </div>
-                <div class="userDetails">
-                <h3>salman khan</h3>
-                <p>salmankhan@gmail.com</p>
-                </div>
-            </div>`
+
+            <div class="userDetails">
+                <h3>${name}</h3>
+                <p>${email}</p>
+    </div>`
 
             userContainer.append(divElement);
 })
