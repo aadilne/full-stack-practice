d3d4d345d88fe98bda21a1bcba37f816c57ea8fb
Aadil Nezam <aadilnezam7782@gmail.com>
2026-09-04 21:10:24 +0530

    Add javaScript in calculator

--- javaScript/10_Mini_Project_DOM/04_Calculator/Calculator.js
+++ javaScript/10_Mini_Project_DOM/04_Calculator/Calculator.js
@@ -1,1 +1,26 @@
 console.log("Calculator.js loaded successfully!");
+
+let display = document.querySelector(".display");
+
+function appendValue(value) {
+
+    if (display.innerText === "0" || display.innerText === "Error" || display.innerText === "Infinity") {
+        display.innerText = value;
+        return;
+    }
+
+    display.innerText = `${display.innerText}${value}`;
+}
+
+function clearDisplay() {
+
+    display.innerText = "0";
+}
+
+function calculate(){
+    try{
+        display.innerText = eval(display.innerText);
+    } catch (error) {
+        display.innerText = "Error";
+    }   
+}
